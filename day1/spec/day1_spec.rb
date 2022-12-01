99f31c198ef259ccebfe98ff3c5395b462752103
Jo Roe <jroe@invoca.com>
2022-12-01 08:18:45 -0800

    regression spec-ing

--- day1/spec/day1_spec.rb
+++ day1/spec/day1_spec.rb
@@ -7,6 +7,10 @@ describe 'day 1' do
     expect(Day1.find_most('day1/data/day1example.txt')).to eq(24000)
   end
 
+  it 'still works for my actual data, part 1' do
+    expect(Day1.find_most('day1/data/day1data.txt')).to eq(75622)
+  end
+
   it 'the total calories of the 3 fattest elves' do
     expect(Day1.find_three_most_total('day1/data/day1example.txt')).to eq(45000)
   end
