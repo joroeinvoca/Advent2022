9dba6c3e596d1f363ae273590fd591af47fed24b
Jo Roe <jroe@invoca.com>
2022-12-01 21:36:19 -0800

    part 2

--- day2/lib/day2.rb
+++ day2/lib/day2.rb
@@ -1,17 +1,4 @@
 
-# A = Rock
-# B = Paper
-# C = Scissors
-#
-# x = Rock = 1
-# Y = Paper = 2
-# Z = Scissors = 3
-#
-# Loss = 0
-# Draw = 3
-# Win = 6
-
-
 class Day2
   @my_choice = {
     'X' => 1,
@@ -37,6 +24,30 @@ class Day2
     }
   }
 
+  @should_result_score = {
+    'X' => 0,
+    'Y' => 3,
+    'Z' => 6
+  }
+
+  @should_choose = {
+    'A' => {
+      'X' => 'Z',
+      'Y' => 'X',
+      'Z' => 'Y'
+    },
+    'B' => {
+      'X' => 'X',
+      'Y' => 'Y',
+      'Z' => 'Z'
+    },
+    'C' => {
+      'X' => 'Y',
+      'Y' => 'Z',
+      'Z' => 'X'
+    }
+  }
+
   def self.total_score(file)
     score = 0
 
@@ -44,6 +55,21 @@ class Day2
       split_line = line.split(' ')
       score += @result_score[split_line[0]][split_line[1]]
       score += @my_choice[split_line[1]]
+    end
+    score
+  end
+
+  def self.total_score2(file)
+    score = 0
+
+    File.foreach(file) do |line|
+      split_line = line.split(' ')
+      # split_line[0] = opponent's play
+      # split_line[1] = result
+      # what I play
+      score += @my_choice[@should_choose[split_line[0]][split_line[1]]]
+      # result
+      score += @should_result_score[split_line[1]]
     end
     score
   end
--- day2/spec/day2_spec.rb
+++ day2/spec/day2_spec.rb
@@ -5,4 +5,12 @@ describe 'day 2' do
   it 'example total score' do
     expect(Day2.total_score('day2/data/day2example.txt')).to eq(15)
   end
+
+  it 'real data total score' do
+    expect(Day2.total_score('day2/data/day2data.txt')).to eq(9177)
+  end
+
+  it 'example data part 2 total score' do
+    expect(Day2.total_score2('day2/data/day2example.txt')).to eq(12)
+  end
 end
