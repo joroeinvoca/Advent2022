31dd66e4f34b7dea5d73906cee4c61e6694111b0
Jo Roe <jroe@invoca.com>
2022-12-03 17:26:27 -0800

    part 2 solution

--- day3/lib/day3.rb
+++ day3/lib/day3.rb
@@ -1,17 +1,28 @@
 
 class Day3
-  def self.sum_of_priorities(file)
-    prio_vals = {}
-    ('a'..'z').each_with_index { |letter, i| prio_vals[letter] = i + 1 }
-    ('A'..'Z').each_with_index { |letter, i| prio_vals[letter] = i + 27 }
+  @prio_vals = {}
+  ('a'..'z').each_with_index { |letter, i| @prio_vals[letter] = i + 1 }
+  ('A'..'Z').each_with_index { |letter, i| @prio_vals[letter] = i + 27 }
 
+  def self.sum_of_priorities(file)
     total = 0
 
     File.foreach(file) do |line|
       stripped = line.strip
       len = stripped.length
       dup_letter = stripped[0, len/2].chars & stripped[len/2, len].chars
-      total += prio_vals[dup_letter[0]]
+      total += @prio_vals[dup_letter[0]]
+    end
+    total
+  end
+
+  def self.sum_of_group_badges(file)
+    total = 0
+    group = []
+
+    File.open(file).each_slice(3) do |lines|
+      dup_letter = lines[0].chars & lines[1].chars & lines[2].chars
+      total += @prio_vals[dup_letter[0]]
     end
     total
   end
--- day3/spec/day3_spec.rb
+++ day3/spec/day3_spec.rb
@@ -5,4 +5,12 @@ describe 'day 3' do
   it 'example data sum' do
     expect(Day3.sum_of_priorities('day3/data/day3example.txt')).to eq(157)
   end
+
+  it 'still works for real data part 1' do
+    expect(Day3.sum_of_priorities('day3/data/day3data.txt')).to eq(7889)
+  end
+
+  it 'example data part 2' do
+    expect(Day3.sum_of_group_badges('day3/data/day3example.txt')).to eq(70)
+  end
 end
