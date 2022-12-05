a1cf11990a00b82a3fb0f60c2836a2ef009dbc85
Jo Roe <jroe@invoca.com>
2022-12-05 09:24:20 -0800

    day 5 part 1 crate results (does not compute final answer yet)

--- day5/lib/day5.rb
+++ day5/lib/day5.rb
@@ -1,6 +1,6 @@
 
 class Day5
-  def self.whats_on_top(file)
+  def self.crate_results(file)
     stacks = {}
 
     File.foreach(file) do |line|
@@ -17,10 +17,29 @@ class Day5
           stack_ind += 1
           line_chars = line_chars.drop(4)
         end
-      elsif !line.strip.empty?
-
+      elsif line.include?('move')
+        instructions = line.split(' ')
+        # instructions[0] => 'move'
+        # [1] => number of crates
+        # [2] => 'from'
+        # [3] => starting stack
+        # [4] => 'to'
+        # [5] => ending stack
+        # int some things
+        instructions[1] = instructions[1].to_i
+        instructions[3] = instructions[3].to_i
+        instructions[5] = instructions[5].to_i
+        # do stuff number of times of crates to move
+        instructions[1].times {
+          # put the crate we're removing (shift) onto the destination stack (unshift)
+          stacks[instructions[5]].unshift(stacks[instructions[3]].shift)
+        }
       end
     end
     stacks
+  end
+
+  def self.crates_on_top(stacks)
+    
   end
 end
--- day5/spec/day5_spec.rb
+++ day5/spec/day5_spec.rb
@@ -2,7 +2,15 @@ require 'rspec'
 require_relative '../lib/day5'
 
 describe 'day 5' do
-  it 'day 5 part 1 example data' do
-    Day5.whats_on_top('day5/data/day5example.txt')
+  result = Day5.crate_results('day5/data/day5example.txt')
+
+  it 'day 5 part 1 crate positions' do
+    expect(result).to include(1 => ['C'])
+    expect(result).to include(2 => ['M'])
+    expect(result).to include(3 => ['Z', 'N', 'D', 'P'])
+  end
+
+  it 'day 5 part 1 crates on top' do
+    expect(Day5.crates_on_top(result)).to eq('CMZ')
   end
 end
