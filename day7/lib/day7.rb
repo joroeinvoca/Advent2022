48825f00621ae48a714241de3ee5acba2c9ece53
Jo Roe <jroe@invoca.com>
2022-12-07 20:32:29 -0800

    fixed for reliability and extendability

--- day7/lib/day7.rb
+++ day7/lib/day7.rb
@@ -1,8 +1,8 @@
 
 class Day7
-  @filesystem = {}
 
-  def self.sum_small_dirs(file)
+  def self.populate_filesystem(file)
+    filesystem = {}
     current_dir = []
 
     File.foreach(file, chomp: true) do |line|
@@ -19,15 +19,20 @@ class Day7
         tmp_dir = ''
         current_dir.each do |dir|
           tmp_dir += '/' + dir
-          if !@filesystem.include?(tmp_dir)
-            @filesystem[tmp_dir] = 0
+          if !filesystem.include?(tmp_dir)
+            filesystem[tmp_dir] = 0
           end
-          @filesystem[tmp_dir] += split_line[0].to_i
+          filesystem[tmp_dir] += split_line[0].to_i
         end
       end
     end
+    filesystem
+  end
+
+  def self.sum_small_dirs(file)
+    filesystem = populate_filesystem(file)
     total = 0
-    @filesystem.each do |folder, size|
+    filesystem.each do |folder, size|
       if size <= 100000
         total += size
       end
