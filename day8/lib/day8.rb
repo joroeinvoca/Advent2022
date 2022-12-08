54d1aff1ac28a86245f8b771603c14c8bcde0116
Jo Roe <jroe@invoca.com>
2022-12-08 10:41:34 -0800

    Day 8: part 1 working (and test for part 2)

--- day8/lib/day8.rb
+++ day8/lib/day8.rb
@@ -1,4 +1,41 @@
 
 class Day8
+  def self.count_visible_trees(file)
+    trees = []
+    row = 0
+    File.foreach(file, chomp: true) do |line|
+      trees[row] = line.chars.map(&:to_i)
+      row += 1
+    end
+    sum = 0
+    #count edge trees
+    sum += 2*trees.size
+    sum += 2*trees[0].size
+    sum -= 4 # haha
 
+    for row in 1 .. trees.size - 2 do
+      for tree in 1 .. trees[0].size - 2 do
+        current = trees[row][tree]
+        if current > trees[row][0 .. tree - 1].max || current > trees[row][tree + 1 .. -1].max
+          sum += 1
+        else
+          above = []
+          below = []
+
+          for i in 0 .. row - 1 do
+            above << trees[i][tree]
+          end
+
+          for i in row + 1 .. trees.size - 1 do
+            below << trees[i][tree]
+          end
+
+          if current > above.max || current > below.max
+            sum += 1
+          end
+        end
+      end
+    end
+    sum
+  end
 end
--- day8/spec/day8_spec.rb
+++ day8/spec/day8_spec.rb
@@ -2,5 +2,11 @@ require 'rspec'
 require_relative '../lib/day8'
 
 describe 'day 8' do
+  it 'part 1 example' do
+    expect(Day8.count_visible_trees('day8/data/day8example.txt')).to eq(21)
+  end
 
+  it 'part 2 example' do
+    expect(Day8.highest_scenic_score('day8/data/day8example.txt')).to eq(8)
+  end
 end
