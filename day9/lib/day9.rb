b7e34d68156c1371f959581c240a3d0555e45790
Jo Roe <jroe@invoca.com>
2022-12-09 21:26:19 -0800

    Day 9 part 2 example works

--- day9/lib/day9.rb
+++ day9/lib/day9.rb
@@ -30,6 +30,53 @@ class Day9
     flatten_hash(spots_touched).length
   end
 
+  def self.long_tail_touch_tally(file)
+    spots_touched = {0 => {0 => 'x'}}
+
+    head_pos = [0, 0]
+    t1_pos = [0, 0]
+    t2_pos = [0, 0]
+    t3_pos = [0, 0]
+    t4_pos = [0, 0]
+    t5_pos = [0, 0]
+    t6_pos = [0, 0]
+    t7_pos = [0, 0]
+    t8_pos = [0, 0]
+    t9_pos = [0, 0]
+
+    File.foreach(file, chomp: true) do |line|
+      inst = line.split(' ')
+      inst[1] = inst[1].to_i
+      inst[1].times do
+        case inst[0]
+        when 'R'
+          head_pos[1] += 1
+        when 'L'
+          head_pos[1] -= 1
+        when 'U'
+          head_pos[0] -= 1
+        when 'D'
+          head_pos[0] += 1
+        end
+        t1_pos = get_new_tail_pos(head_pos, t1_pos)
+        t2_pos = get_new_tail_pos(t1_pos, t2_pos)
+        t3_pos = get_new_tail_pos(t2_pos, t3_pos)
+        t4_pos = get_new_tail_pos(t3_pos, t4_pos)
+        t5_pos = get_new_tail_pos(t4_pos, t5_pos)
+        t6_pos = get_new_tail_pos(t5_pos, t6_pos)
+        t7_pos = get_new_tail_pos(t6_pos, t7_pos)
+        t8_pos = get_new_tail_pos(t7_pos, t8_pos)
+        t9_pos = get_new_tail_pos(t8_pos, t9_pos)
+
+        if !spots_touched[t9_pos[0]]
+          spots_touched[t9_pos[0]] = {}
+        end
+        spots_touched[t9_pos[0]][t9_pos[1]] = 'x'
+      end
+    end
+    flatten_hash(spots_touched).length
+  end
+
   def self.flatten_hash(param, prefix=nil)
     param.each_pair.reduce({}) do |a, (k, v)|
       v.is_a?(Hash) ? a.merge(flatten_hash(v, "#{prefix}#{k}.")) : a.merge("#{prefix}#{k}".to_sym => v)
--- day9/spec/day9_spec.rb
+++ day9/spec/day9_spec.rb
@@ -5,4 +5,12 @@ describe 'day 9' do
   it 'part 1 does something' do
     expect(Day9.tail_touch_tally('day9/data/day9example.txt')).to eq(13)
   end
+
+  it 'part 1 works with real data too' do
+    expect(Day9.tail_touch_tally('day9/data/day9data.txt')).to eq(6011)
+  end
+
+  it 'part 2 example data' do
+    expect(Day9.long_tail_touch_tally('day9/data/day9example.txt')).to eq(1)
+  end
 end
