c7713650aae3529bc5163b8ca563e271084b730a
Jo Roe <jroe@invoca.com>
2022-12-14 20:39:54 -0800

    Day 11 part 1 example working

--- day11/lib/day11.rb
+++ day11/lib/day11.rb
@@ -4,6 +4,7 @@ class Day11
     monkey_items = {}
     monkey_op = {}
     monkey_test = {}
+    objects_inspected = {}
 
     total_cycles = 20
 
@@ -15,29 +16,35 @@ class Day11
       monkey_items[monkey] = chunk[1].partition(':')[2].split(', ').map!(&:strip).map!(&:to_i)
       # populate monkey rules
       monkey_op[monkey] = chunk[2].partition(':')[2].split(' ')
-      monkey_op[monkey][4] = monkey_op[monkey][4].to_i
+      monkey_op[monkey][4] = monkey_op[monkey][4] == 'old' ? 'old' : monkey_op[monkey][4].to_i
       # populate monkey test
       div = chunk[3].partition(':')[2].split(' ')[2].to_i
       if_t = chunk[4].partition(":")[2].split(' ')[3].to_i
       if_f = chunk[5].partition(":")[2].split(' ')[3].to_i
       monkey_test[monkey] = [div, if_t, if_f]
+      # populate objects inspected
+      objects_inspected[monkey] = 0
     end
 
     # figure out where item goes
     current_monkey = 0
-    total_cycles.downto(1) do
+    total_cycles.downto(1) do |cycle|
+      current_monkey = 0
       while current_monkey <= monkey_items.keys.max
-        # Monkey inspects an item
-        current_item = monkey_items[current_monkey].shift
-        # monkey_op changes worry level
-        current_item = current_item.method(monkey_op[current_monkey][3]).(monkey_op[current_monkey][4])
-        # Worry level is divided by 3 (and rounded down)
-        current_item = (current_item / 3).to_i
-        # eval rule, item (with new worry level) passed to true or false monkey (to end of list)
-        if current_item % monkey_test[current_monkey][0] == 0
-          monkey_items[monkey_test[current_monkey][1]].push(current_item)
-        else
-          monkey_items[monkey_test[current_monkey][2]].push(current_item)
+        if monkey_items[current_monkey].size > 0
+          objects_inspected[current_monkey] += 1
+          # Monkey inspects an item
+          current_item = monkey_items[current_monkey].shift
+          # monkey_op changes worry level
+          current_item = monkey_math(current_item, monkey_op[current_monkey][3], monkey_op[current_monkey][4])
+          # Worry level is divided by 3 (and rounded down)
+          current_item = (current_item / 3).to_i
+          # eval rule, item (with new worry level) passed to true or false monkey (to end of list)
+          if current_item % monkey_test[current_monkey][0] == 0
+            monkey_items[monkey_test[current_monkey][1]].push(current_item)
+          else
+            monkey_items[monkey_test[current_monkey][2]].push(current_item)
+          end
         end
         if monkey_items[current_monkey].size < 1
           current_monkey += 1
@@ -45,8 +52,16 @@ class Day11
       end
     end
 
-
     # multiply top 2 quantities of items inspected
-    puts 'end'
+    busy_monkeys = objects_inspected.values.max(2)
+    busy_monkeys[0] * busy_monkeys[1]
+  end
+
+  def self.monkey_math(cur, op, val)
+    if val == 'old'
+      return cur.method(op).(cur)
+    else
+      return cur.method(op).(val)
+    end
   end
 end
