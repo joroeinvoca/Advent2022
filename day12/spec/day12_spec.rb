10f4cceeaae3b66ea25ae077047fa6b89c9d99f7
Jo Roe <jroe@invoca.com>
2022-12-16 20:35:55 -0800

    test proving real data doesn't work

--- day12/spec/day12_spec.rb
+++ day12/spec/day12_spec.rb
@@ -5,4 +5,8 @@ describe 'day 12' do
   it 'lowest number of steps in example (part 1)' do
     expect(Day12.find_path('day12/data/day12example.txt')).to eq(31)
   end
+
+  it 'real data part 1' do
+    expect(Day12.find_path('day12/data/day12data.txt')).not_to be_nil
+  end
 end
