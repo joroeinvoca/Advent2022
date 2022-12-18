7b524f1437614a2fdc8cf35b6e3e600846274d12
Jo Roe <jroe@invoca.com>
2022-12-18 10:32:01 -0800

    Day 13 part 1 example solution

--- day13/lib/day13.rb
+++ day13/lib/day13.rb
@@ -1,33 +1,52 @@
 
 class Day13
-  # compare ints, lower should be first
-  #
-  # compare lists, see above, first should run out of items first
-  #
-  # if comparing to lonely int, give it some []
-
   def self.sum_correct_indicies(file)
     cur_index = 1
     input_pairs = {}
     final_sum = 0
 
     File.open(file).each_slice(3) do |lines|
-      input_pairs[cur_index] = [lines[0].strip, lines[1].strip]
+      input_pairs[cur_index] = [eval(lines[0].strip), eval(lines[1].strip)]
       cur_index += 1
     end
 
     for i in 1 .. input_pairs.keys.max do
-      if compare_packets(input_pairs[i][0], input_pairs[i][1])
+      if is_order_correct(input_pairs[i][0], input_pairs[i][1])
         final_sum += i
       end
     end
     final_sum
   end
 
-  def self.compare_packets(pac_left, pac_right)
-    
+  def self.is_order_correct(pac_left, pac_right)
+    result = nil
+    for i in 0 .. [pac_left.size, pac_right.size].min - 1 do
+      if pac_left[i].kind_of?(Array) && pac_right[i].kind_of?(Array)
+        # if both lists, check values in lists
+        result = is_order_correct(pac_left[i], pac_right[i])
+      elsif pac_left[i].kind_of?(Array)
+        result = is_order_correct(pac_left[i], [pac_right[i]])
+      elsif pac_right[i].kind_of?(Array)
+        result = is_order_correct([pac_left[i]], pac_right[i])
+      elsif pac_left[i].kind_of?(Integer) && pac_right[i].kind_of?(Integer)
+        # if both ints
+        if pac_left[i] == pac_right[i]
+          # same int, check next value
+          next
+        elsif pac_left[i] < pac_right[i]
+          # left int < right int, order is correct
+          return true
+        elsif pac_left[i] > pac_right[i]
+          # left int > right int, order is wrong
+          return false
+        end
+      end
+    end
+    if pac_left.size < pac_right.size
+      result = true
+    elsif pac_left.size > pac_right.size
+      result = false
+    end
+    result
   end
-
-
-  # return sum of indexes of correct pairs
 end
