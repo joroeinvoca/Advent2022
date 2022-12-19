3cfcfca509ca864d9f56043bd556d30251bce5f2
Jo Roe <jroe@invoca.com>
2022-12-19 11:15:33 -0800

    part 2 example working, also added debug print

--- day14/lib/day14.rb
+++ day14/lib/day14.rb
@@ -1,6 +1,6 @@
 
 class Day14
-  def self.how_much_sand(file)
+  def self.how_much_sand(file, add_floor=false)
     cave_map = {}
 
     File.foreach(file, chomp:true) do |line|
@@ -11,7 +11,6 @@ class Day14
 
           # horizontal line
           if start_coords[1] == end_coords[1]
-            #high_point = [start_coords[0], end_coords[0]].min
             for vert in [start_coords[0], end_coords[0]].min .. [start_coords[0], end_coords[0]].max do
               if !cave_map[start_coords[1]]
                 cave_map[start_coords[1]] = {}
@@ -35,6 +34,15 @@ class Day14
     # need to put the first empty row in the cave_map
     cave_map[0] = {}
 
+    # optionally add the floor
+    if add_floor
+      lowest_level = cave_map.keys.max
+      cave_map[lowest_level + 2] = {}
+      for floor in cave_map[lowest_level].keys.min - 50 .. cave_map[lowest_level].keys.max + 50 do
+        cave_map[lowest_level + 2][floor] = '#'
+      end
+    end
+
     grains = 0
     done = false
     loop do
@@ -56,6 +64,10 @@ class Day14
           # sand has landed
           else
             cave_map[sand_pos[1]][sand_pos[0]] = 'o'
+            if sand_pos == [500, 0]
+              grains += 1
+              break done = true
+            end
           end
         # also down, but filling out empty cave map
         else
@@ -70,6 +82,7 @@ class Day14
         if (sand_pos[1] + 1) > cave_map.keys.max
           break done = true
         end
+
       end
 
       if done
@@ -77,7 +90,22 @@ class Day14
       end
 
       grains += 1
-      end
+    end
+    print_cave(cave_map)
     grains
+  end
+
+  def self.print_cave(cave_map)
+    for row in cave_map.keys.min .. cave_map.keys.max do
+      for col in cave_map[cave_map.keys.max].keys.min .. cave_map[cave_map.keys.max].keys.max do
+        if cave_map[row][col]
+          print(cave_map[row][col])
+        else
+          print('.')
+        end
+      end
+      print("\n")
+    end
+    print("\n")
   end
 end
--- day14/spec/day14_spec.rb
+++ day14/spec/day14_spec.rb
@@ -5,4 +5,12 @@ describe 'day 14' do
   it 'part 1 example' do
     expect(Day14.how_much_sand('day14/data/day14example.txt')).to eq(24)
   end
+
+  it 'part 1 data' do
+    expect(Day14.how_much_sand('day14/data/day14data.txt')).to eq(1199)
+  end
+
+  it 'part 2 example' do
+    expect(Day14.how_much_sand('day14/data/day14example.txt', true)).to eq(93)
+  end
 end
