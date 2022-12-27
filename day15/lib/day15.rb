08fadbae877c18acedba551bec74b512f01d6d17
Jo Roe <jroe@invoca.com>
2022-12-26 19:25:29 -0800

    trying calculating by sections

--- day15/lib/day15.rb
+++ day15/lib/day15.rb
@@ -24,7 +24,6 @@ class Day15
 
       dist = calc_dist(sensor_x, sensor_y, beacon_x, beacon_y)
       if [*sensor_y - dist .. sensor_y + dist].include? row
-
         for x in (sensor_x - dist) .. (sensor_x + dist)
           if calc_dist(sensor_x, sensor_y, x, row) <= dist
             populate_map(x, row, '#')
@@ -33,7 +32,6 @@ class Day15
             end
           end
         end
-
       end
     end
     for i in @beacon_map[row].keys.min .. @beacon_map[row].keys.max
@@ -44,6 +42,71 @@ class Day15
     covered_spots
   end
 
+  def self.find_tuning_freq(file, max_coord, min_coord=0)
+    @beacon_map = {}
+    full_rows = []
+
+    File.foreach(file, chomp: true) do |line|
+      puts line
+      # sensor
+      sensor_x = line[12 .. line.index(',') - 1].to_i
+      sensor_y = line[line.index('y=') + 2 .. line.index(':') - 1].to_i
+      if [*0 .. max_coord].include?(sensor_x) && [*min_coord .. max_coord].include?(sensor_y) && !full_rows.include?(sensor_y)
+        populate_map(sensor_x, sensor_y, 'S')
+      end
+
+      # beacon
+      beacon_x = line[line.index('x=', line.index(':')) + 2 .. line.index(', y=', line.index(':')) - 1].to_i
+      beacon_y = line[line.index('y=', line.index(':')) + 2 .. -1].to_i
+      if [*0 .. max_coord].include?(beacon_x) && [*min_coord .. max_coord].include?(beacon_y) && !full_rows.include?(beacon_y)
+        populate_map(beacon_x, beacon_y, 'B')
+      end
+
+      dist = calc_dist(sensor_x, sensor_y, beacon_x, beacon_y)
+      puts "dist: #{dist}"
+
+      for y in [(sensor_y - dist), min_coord].max .. [(sensor_y + dist), max_coord].min
+        if full_rows.include?(y) || (@beacon_map[y] && @beacon_map[y].values.count > max_coord)
+          full_rows << y
+          puts "full row: #{y}"
+          @beacon_map.delete(y)
+          next
+        end
+        puts "processing y: #{y}"
+        for x in [(sensor_x - dist), 0].max .. [(sensor_x + dist), max_coord].min
+          if calc_dist(sensor_x, sensor_y, x, y) <= dist
+            populate_map(x, y, '#')
+          end
+        end
+      end
+    end
+
+    @beacon_map.each do |row_num, x_vals|
+      puts row_num
+      if x_vals.count == max_coord
+        return calc_freq(row_num, [*0 .. max_coord + 1] - x_vals.keys)
+      end
+    end
+    nil
+  end
+
+  def self.find_freq_sections(file, max_coord, number_of_sections)
+    section_size = max_coord / number_of_sections
+
+    answer = nil
+    sections = []
+
+    for i in 0 .. number_of_sections - 1
+      answer = find_tuning_freq(file, section_size * (i + 1), section_size * i)
+      if answer
+        return answer
+      end
+    end
+  end
+
+  def self.calc_freq(y, x)
+    x[0] * 4000000 + y
+  end
 
   def self.populate_map(x, y, label)
     if !@beacon_map[y]
--- day15/spec/day15_spec.rb
+++ day15/spec/day15_spec.rb
@@ -6,7 +6,19 @@ describe 'day 15' do
     expect(Day15.beaconless_locations('day15/data/day15example.txt', 10, false)).to eq(26)
   end
 
-  it 'part 1 real data y=2000000' do
+  xit 'part 1 real data y=2000000' do
     expect(Day15.beaconless_locations('day15/data/day15data.txt', 2000000)).to eq(4879972)
+  end
+
+  it 'part 2 tuning frequency example' do
+    expect(Day15.find_tuning_freq('day15/data/day15example.txt', 20)).to eq(56000011)
+  end
+
+  it 'part 2 example split up' do
+    expect(Day15.find_freq_sections('day15/data/day15example.txt', 20, 2)).to eq(56000011)
+  end
+
+  xit 'part 2 real data' do
+    expect(Day15.find_tuning_freq('day15/data/day15data.txt', 4000000))
   end
 end
