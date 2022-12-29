3a9a931365157b4be0790cb012cf431c28b91c0b
Jo Roe <jroe@invoca.com>
2022-12-28 19:11:08 -0800

    cleaning up failed attempts

--- day15/lib/day15.rb
+++ day15/lib/day15.rb
@@ -42,55 +42,6 @@ class Day15
     covered_spots
   end
 
-  def self.find_tuning_freq(file, max_coord, min_coord=0)
-    @beacon_map = {}
-    full_rows = []
-
-    File.foreach(file, chomp: true) do |line|
-      puts line
-      # sensor
-      sensor_x = line[12 .. line.index(',') - 1].to_i
-      sensor_y = line[line.index('y=') + 2 .. line.index(':') - 1].to_i
-      if [*0 .. max_coord].include?(sensor_x) && [*min_coord .. max_coord].include?(sensor_y) && !full_rows.include?(sensor_y)
-        populate_map(sensor_x, sensor_y, 'S')
-      end
-
-      # beacon
-      beacon_x = line[line.index('x=', line.index(':')) + 2 .. line.index(', y=', line.index(':')) - 1].to_i
-      beacon_y = line[line.index('y=', line.index(':')) + 2 .. -1].to_i
-      if [*0 .. max_coord].include?(beacon_x) && [*min_coord .. max_coord].include?(beacon_y) && !full_rows.include?(beacon_y)
-        populate_map(beacon_x, beacon_y, 'B')
-      end
-
-      dist = calc_dist(sensor_x, sensor_y, beacon_x, beacon_y)
-      puts "dist: #{dist}"
-
-      for y in [(sensor_y - dist), min_coord].max .. [(sensor_y + dist), max_coord].min
-        if full_rows.include?(y) || (@beacon_map[y] && @beacon_map[y].values.count > max_coord)
-          full_rows << y
-          puts "full row: #{y}"
-          @beacon_map.delete(y)
-          next
-        end
-        puts "processing y: #{y}"
-        for x in [(sensor_x - dist), 0].max .. [(sensor_x + dist), max_coord].min
-          if calc_dist(sensor_x, sensor_y, x, y) <= dist
-            populate_map(x, y, '#')
-          end
-        end
-      end
-    end
-
-    @beacon_map.each do |row_num, x_vals|
-      puts row_num
-      if x_vals.count == max_coord
-        special_x_vals = [*0 .. max_coord + 1] - x_vals.keys
-        return calc_freq(row_num, special_x_vals[0])
-      end
-    end
-    nil
-  end
-
   def self.find_freq_ranges(file, max_coord)
     @beacon_map = {}
     @full_row_ranges = []
@@ -119,7 +70,6 @@ class Day15
             next
           end
         end
-
 
         end_vals = [dist - (y_val - sensor_y).abs + sensor_x, - dist + (y_val - sensor_y).abs + sensor_x]
         x_rng = keep_range_within_bounds((end_vals.min .. end_vals.max), max_coord)
@@ -157,20 +107,6 @@ class Day15
       end
     end
     false
-  end
-
-  def self.find_freq_sections(file, max_coord, number_of_sections)
-    section_size = max_coord / number_of_sections
-
-    answer = nil
-    sections = []
-
-    for i in 0 .. number_of_sections - 1
-      answer = find_tuning_freq(file, section_size * (i + 1), section_size * i)
-      if answer
-        return answer
-      end
-    end
   end
 
   def self.calc_freq(y, x)
