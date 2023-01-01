cf66bfa42b21b260c57e0822083e57597d613e3d
Jo Roe <jroe@invoca.com>
2022-12-31 16:16:44 -0800

    part 1 example data passes in ~26 seconds

--- day16/lib/day16.rb
+++ day16/lib/day16.rb
@@ -32,13 +32,33 @@ class Day16
     routes[0] = ['AA'].fill(2, minutes - 1) { nil }
     routes[0] << 0
 
+    useful_valves = valve_flow.filter { |valve, flow| flow > 0 }.keys.map! { |valve| valve + '1' }
+
     for min in 1 .. minutes
       puts "minute: #{min}"
       puts "routes: #{routes.size}"
+
+      # get rid of bum routes
+      routes.delete_if do |route_to_check|
+        route_to_check.include?('$')
+      end
+
+      # eventually, just drop the worst routes
+      if min > 5
+        # delete lowest 1/3?
+        cutoff = routes.map(&:last).min(routes.count / 2).last
+        routes.delete_if do |route_to_delete|
+          route_to_delete.last < cutoff
+        end
+      end
+
       routes.each_with_index do |route, i|
         # don't reprocess new routes that have this minute already done
         if !route[min].nil?
           next
+        else
+          # add pressure from current minute if we need to process this one still
+          routes[i][-1] = recalc_pressure(valve_flow, route)
         end
 
         current_valve = route[min - 1][0, 2]
@@ -46,20 +66,26 @@ class Day16
         if !route.include?(current_valve + '1') && valve_flow[current_valve] > 0
           new_route = route.map(&:clone)
           new_route[min] = current_valve + '1'
-          new_route[-1] = recalc_pressure(valve_flow, new_route)
+          #new_route[-1] = recalc_pressure(valve_flow, new_route)
           routes << new_route
         end
 
         # see how many tunnel options there are, try them all
+        # but not if they go to an already-on dead end
         valve_map[current_valve].each_with_index do |tunnel, ind|
+          if valve_map[tunnel].size == 1 && valve_map[tunnel][0] == current_valve && (route.include?(tunnel + '1') || valve_flow[tunnel] == 0)
+            # skip this one from now on
+            route[min] = '$'
+            next
+          end
           if ind < valve_map[current_valve].size - 1
             new_route = route.map(&:clone)
             new_route[min] = tunnel
-            new_route[-1] = recalc_pressure(valve_flow, new_route)
+            #new_route[-1] = recalc_pressure(valve_flow, new_route)
             routes << new_route
           else
             routes[i][min] = tunnel
-            routes[i][-1] = recalc_pressure(valve_flow, route)
+            #routes[i][-1] = recalc_pressure(valve_flow, route)
           end
         end
       end
@@ -72,7 +98,9 @@ class Day16
     new_pressure = route[-1]
 
     for i in 0 .. route.size - 2
-      if route[i] && route[i].length == 3
+      if route[i].nil?
+        next
+      elsif route[i].length == 3
         new_pressure += valve_flow[route[i][0, 2]]
       end
     end
